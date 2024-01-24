8471f8965f08710e6badcfb69423b4e28b7c233b
innopanda <137259872+innopanda@users.noreply.github.com>
2024-01-24 09:41:20 +0800

    Initial commit with my portfolio files

---
+++ my-work-portfolio/script.js
@@ -0,0 +1,26 @@
+
+function toggleBio() {
+    var bio = document.getElementById("bio");
+    if (bio.style.display === "none" || bio.style.display === "") {
+        bio.style.display = "block";
+    } else {
+        bio.style.display = "none";
+    }
+}
+
+function updatePortfolio(folderName) {
+    for (let i = 1; i <= 20; i++) {
+        let imgElement = document.getElementById('image' + i).getElementsByTagName('img')[0];
+        imgElement.src = folderName + '/Job' + i + '.png';
+        
+
+        let container = document.getElementById('image' + i);
+        container.getElementsByTagName('h2')[0].textContent = folderName + i;
+        container.getElementsByTagName('p')[0].textContent = '' + i;
+    }
+}
+
+// 页面加载时调用这个函数来初始化显示的作品集
+window.onload = function() {
+    updatePortfolio('空间陈列设计'); // 只需更改这里的类别名称即可
+};
